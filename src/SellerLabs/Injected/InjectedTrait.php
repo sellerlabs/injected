4afafc5af8ae73aeaa9104f7aba65b325e9a4d84
Benjamin Kovach <bkovach13@gmail.com>
2015-12-04 08:01:47 -0500

    Remove copyright

--- src/SellerLabs/Injected/InjectedTrait.php
+++ src/SellerLabs/Injected/InjectedTrait.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * Copyright 2014-2015, SellerLabs
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * This file is part of the Snagshout package
- */
-
 namespace SellerLabs\Injected;
 
 use Exception;
